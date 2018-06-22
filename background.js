7d8f10a3df6f0f07b2e88949affe3320b0e64549
LAXMANN A/L RAVINDRAN <laxmannturbo@gmail.com>
2018-06-23 01:11:42 +0800

    Declared Function edited by me.

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+//function edited by laxmann
+//view.html page has been created by laxmann.
 function openPage() {
   browser.tabs.create({
     url: "view.html" // view html will open the main page 
